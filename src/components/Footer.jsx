d130fffe8db116b15fc4765c6ddce793bc2d7776
Jaimin Hapani <jaiminhapani9960@gmail.com>
2023-08-22 19:20:41 +0530

    "change color of divider"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ function Footer() {
                 </div>
             </div>
 
-            <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+            <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
                 <p className={`font-poppins font-normal text-center text-[18px] leading-[27px] text-dimWhite`}>
                     Copyright &copy; 2021 HooBank. All Rights Reserved.
                 </p>
